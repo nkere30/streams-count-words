9c7f87888e82c426945dd30c12b422cc36dea4e6
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-12-22 21:52:50 +0400

    Commit N1

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,12 +1,29 @@
 package com.epam.rd.autotasks;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
-
-        return null;
+        String wordsStatistics = lines
+                .stream()
+                .map((line) -> line.split("[\"\\s-‘/'’”“$&+,:;=?@#|<>.^*()%!—-]"))
+                .flatMap(Arrays::stream)
+                .map(String::toLowerCase)
+                .filter((word) -> word.length() >= 4)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry::getKey))
+                .filter((entry) -> entry.getValue() >= 10)
+                .map((entry) -> entry.getKey() + " - " + entry.getValue() + "\n")
+                .collect(Collectors.joining());
+        return wordsStatistics.substring(0, wordsStatistics.length() - 1);
     }
 }
